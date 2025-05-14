df6d014d310160161b2c8448c9243477a6c46bc1
MinsungKim <alstjd025@gmail.com>
2025-05-14 11:47:13 +0900

    Add gitignores
    Changes to be committed:
    	modified:   .gitignore
    	new file:   tensorflow/lite/.vscode/c_cpp_properties.json
    	new file:   tensorflow/lite/.vscode/settings.json
    	modified:   tensorflow/lite/examples/simple_test/simple_test.cc

--- tensorflow/lite/examples/simple_test/simple_test.cc
+++ tensorflow/lite/examples/simple_test/simple_test.cc
@@ -13,6 +13,17 @@
 #include "tensorflow/lite/model_builder.h"
 #include "tensorflow/lite/optional_debug_tools.h"
 
+// For colors
+#define RESET       "\033[0m"
+#define BLACK       "\033[30m"
+#define RED         "\033[31m"
+#define GREEN       "\033[32m"
+#define YELLOW      "\033[33m"
+#define BLUE        "\033[34m"
+#define MAGENTA     "\033[35m"
+#define CYAN        "\033[36m"
+#define WHITE       "\033[37m"
+
 /* Note: Minsung cmt. No GPU usage. */
 // #include "tensorflow/lite/delegates/gpu/delegate.h"
 
